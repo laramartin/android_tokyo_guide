f35e143c985b4d51a1d062ceef8188dc5b6cc147
Lara Martin <lara.martin.carretero@gmail.com>
2016-08-28 19:42:07 +0200

    sights list, init list

---
+++ app/src/main/java/eu/laramartin/toykoguide/Sights.java
@@ -0,0 +1,28 @@
+package eu.laramartin.toykoguide;
+
+import java.util.List;
+
+/**
+ * Created by lara on 28/8/16.
+ */
+public class Sights {
+
+    public static void initSightsList(List<Location> list) {
+
+
+    }
+
+
+   /* public Location(String name, String description, String address, String phone, String schedule,
+                    String price, int imageResourceId) {
+        this.name = name;
+        this.description = description;
+        this.address = address;
+        this.phone = phone;
+        this.schedule = schedule;
+        this.price = price;
+        this.imageResourceId = imageResourceId;
+    }*/
+
+
+}
