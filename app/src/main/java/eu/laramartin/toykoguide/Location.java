cd481df957154ce52d4b3d6af813c4b05f675005
Lara Martin <lara.martin.carretero@gmail.com>
2016-08-28 19:33:06 +0200

    getters setters. imageResourceId init as -1

--- app/src/main/java/eu/laramartin/toykoguide/Location.java
+++ app/src/main/java/eu/laramartin/toykoguide/Location.java
@@ -1,17 +1,20 @@
 package eu.laramartin.toykoguide;
 
+import android.util.Log;
+
 /**
  * Created by lara on 28/8/16.
  */
 public class Location {
 
-    String name;
-    String description;
-    String address;
-    String phone;
-    String schedule;
-    String price;
-    int imageResourceId;
+    private String name;
+    private String description;
+    private String address;
+    private String phone;
+    private String schedule;
+    private String price;
+    private static final int NO_IMAGE_PROVIDED = -1;
+    private int imageResourceId = NO_IMAGE_PROVIDED;
 
     public Location(String name, String description, String address, String phone, String schedule,
                     String price, int imageResourceId) {
@@ -33,5 +36,67 @@ public class Location {
         this.price = price;
         this.imageResourceId = imageResourceId;
     }
-    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(String schedule) {
+        this.schedule = schedule;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public void setImageResourceId(int imageResourceId) {
+        this.imageResourceId = imageResourceId;
+    }
+
+    public boolean hasImage(){
+        Log.v("hasImage", "word has image: "+ (imageResourceId != NO_IMAGE_PROVIDED));
+        return imageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+
 }
