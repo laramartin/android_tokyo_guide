192a93ffd5f58d3c3820eac2b1bf92d5486b4059
Lara Martin <lara.martin.carretero@gmail.com>
2016-08-28 20:03:29 +0200

    printing the first location

--- app/src/main/java/eu/laramartin/toykoguide/MainActivity.java
+++ app/src/main/java/eu/laramartin/toykoguide/MainActivity.java
@@ -2,12 +2,23 @@ package eu.laramartin.toykoguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Location> list = new ArrayList<>();
+        Sights.initSightsList(list);
+
+        Log.v("MainActivity", list.get(0).toString());
     }
 }
