40545e802d083607a0a72a56618d40289fa9231e
Lara Martin <lara.martin.carretero@gmail.com>
2016-08-29 22:23:54 +0200

    set adapter, inflate views

--- app/src/main/java/eu/laramartin/toykoguide/MainActivity.java
+++ app/src/main/java/eu/laramartin/toykoguide/MainActivity.java
@@ -1,24 +1,32 @@
 package eu.laramartin.toykoguide;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.locations_list);
 
         List<Location> list = new ArrayList<>();
         Sights.initSightsList(list);
 
         Log.v("MainActivity", list.get(0).toString());
+
+
+        LocationAdapter adapter = new LocationAdapter(this, -1, list);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        listView.setAdapter(adapter);
+
+
     }
 }
