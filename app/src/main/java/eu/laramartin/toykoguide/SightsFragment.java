7f6edaa779f6d1e41a0268ef10892997f500c743
Lara Martin <lara.martin.carretero@gmail.com>
2016-09-01 21:35:43 +0200

    simple fragment

--- app/src/main/java/eu/laramartin/toykoguide/SightsFragment.java
+++ app/src/main/java/eu/laramartin/toykoguide/SightsFragment.java
@@ -3,10 +3,14 @@ package eu.laramartin.toykoguide;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by lara on 1/9/16.
@@ -18,29 +22,21 @@ public class SightsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 
-        /*List<Location> list = new ArrayList<>();
+        List<Location> list = new ArrayList<>();
         Sights.initSightsList(list);
 
         Log.v("MainActivity", list.get(0).toString());
 
 
         LocationAdapter adapter = new LocationAdapter(getActivity(), -1, list);
-        View view = super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.locations_list, container, false);
         ListView listView = (ListView) view.findViewById(R.id.listView);
 
         listView.setAdapter(adapter);
 
-        return view;*/
-        ViewPager = (ViewPager) findViewById(R.id.pager);
-        ViewPager.setOnPageChangeListener(
-                new ViewPager.SimpleOnPageChangeListener() {
-                    @Override
-                    public void onPageSelected(int position) {
-                        // When swiping between pages, select the
-                        // corresponding tab.
-                        getActionBar().setSelectedNavigationItem(position);
-                    }
-                });
+        return view;
+
+
 
     }
 }
