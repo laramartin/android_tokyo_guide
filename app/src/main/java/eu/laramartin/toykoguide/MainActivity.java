312fa9a87934fca1184b160e2fea86e76d755265
Lara Martin <lara.martin.carretero@gmail.com>
2016-09-02 19:56:57 +0200

    added tabs

--- app/src/main/java/eu/laramartin/toykoguide/MainActivity.java
+++ app/src/main/java/eu/laramartin/toykoguide/MainActivity.java
@@ -3,6 +3,8 @@ package eu.laramartin.toykoguide;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.TabLayout;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
       /*  viewPager.setOnPageChangeListener(
                 new ViewPager.SimpleOnPageChangeListener() {
