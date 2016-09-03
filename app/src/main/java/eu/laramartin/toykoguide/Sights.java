99e437790a9b26e2624d86938fbf6bad9a5f819d
Lara Martin <lara.martin.carretero@gmail.com>
2016-09-03 12:58:19 +0200

    added 2 sights with drawables

--- app/src/main/java/eu/laramartin/toykoguide/Sights.java
+++ app/src/main/java/eu/laramartin/toykoguide/Sights.java
@@ -20,23 +20,27 @@ public class Sights {
         ));
 
         list.add(new Location(
-                "Ghibli Museum",
-                "Museum showcasing the work of the Japanese animation studio Studio Ghibli.",
-                "1-1-83 Simorenjaku, Mitaka-shi Tokyo 181-0013",
-                "+81 570-055-777",
-                "10:00–18:00. Tuesdays closed.",
-                "110-330¥",
-                R.drawable.sights_ghibli_museum
+                "Tokyo Imperial Palace",
+                "It is a large park-like area that contains buildings including the main palace, " +
+                        "the private residences of the Imperial Family, an archive, " +
+                        "museums and administrative offices.",
+                "1-1 Chiyoda, Tokyo 100-8111",
+                "+81 3-3213-1111",
+                "9:00–17:00. Mondays & Fridays closed.",
+                "Free",
+                R.drawable.sights_imperial_palace
         ));
 
         list.add(new Location(
-                "Ghibli Museum",
-                "Museum showcasing the work of the Japanese animation studio Studio Ghibli.",
-                "1-1-83 Simorenjaku, Mitaka-shi Tokyo 181-0013",
-                "+81 570-055-777",
-                "10:00–18:00. Tuesdays closed.",
-                "110-330¥",
-                R.drawable.sights_ghibli_museum
+                "Yasukuni Shrine",
+                "Shinto shrine founded by Emperor Meiji. Commemorates those who died in service " +
+                        "of the Empire of Japan, which existed from the Meiji Restoration of " +
+                        "1869 until the nation was renamed during the Allied occupation in 1947.",
+                "3-1-1 Kudankita, Chiyoda, Tokyo 102-8246",
+                "+81 3-3261-8326",
+                "Always open",
+                "Free",
+                R.drawable.sights_yasukuni_shrine
         ));
 
         list.add(new Location(
