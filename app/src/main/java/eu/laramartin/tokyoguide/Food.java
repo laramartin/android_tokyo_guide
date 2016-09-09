c1e3addcb3a8ea1d9ecc4f404d51fa72f15abe14
Lara Martin <lara.martin.carretero@gmail.com>
2016-09-10 00:13:15 +0200

    food list hardcoded strings to strings.xml

--- app/src/main/java/eu/laramartin/tokyoguide/Food.java
+++ app/src/main/java/eu/laramartin/tokyoguide/Food.java
@@ -1,5 +1,7 @@
 package eu.laramartin.tokyoguide;
 
+import android.content.Context;
+
 import java.util.List;
 
 /**
@@ -7,80 +9,76 @@ import java.util.List;
  */
 public class Food {
 
-    public static void initFoodsList(List<Location> list) {
+    public static void initFoodsList(List<Location> list, Context context) {
 
         list.add(new Location(
-                "Genki Sushi",
-                "Sushi restaurant. Choose the food on the screen in your table and " +
-                        "recieve your order via conveyor belt",
-                "24-8 Udagawacho, Shibuya, Tokyo 150-0042",
-                "+81 3-3461-1281",
-                "Mo-Fr 11:00–24:00. Sa-Su 10:30-24:00",
-                "$$",
+                context.getString(R.string.food_genki_name),
+                context.getString(R.string.food_genki_description),
+                context.getString(R.string.food_genki_address),
+                context.getString(R.string.food_genki_phone),
+                context.getString(R.string.food_genki_schedule),
+                context.getString(R.string.food_price_two),
                 R.drawable.food_genki_sushi
         ));
 
         list.add(new Location(
-                "Tonkatsu Wako",
-                "Restaurant specialised in Tonkatsu, fried pork cutlet.",
-                "AQUA CiTY ODAIBA, 1 Chome-7-1 Daiba, Minatoku Tokyo 135-0091",
-                "+81 3-3599-5144",
-                "Mo-Su 11:00–22:00",
-                "$$",
+                context.getString(R.string.food_wako_name),
+                context.getString(R.string.food_wako_description),
+                context.getString(R.string.food_wako_address),
+                context.getString(R.string.food_wako_phone),
+                context.getString(R.string.food_wako_schedule),
+                context.getString(R.string.food_price_two),
                 R.drawable.food_tonkatsu_wako
         ));
 
         list.add(new Location(
-                "Rokumonya",
-                "Restaurant specialised in Okonomiyaki, a Japanese savoury pancake containing " +
-                        "a variety of ingredients",
-                "3 Chome−35, Shinjuku, Tokyo 160-0022",
-                "+81 3-3356-6824",
-                "Mo-Su 11:30–23:30",
-                "$$",
+                context.getString(R.string.food_rokumonya_name),
+                context.getString(R.string.food_rokumonya_description),
+                context.getString(R.string.food_rokumonya_address),
+                context.getString(R.string.food_rokumonya_phone),
+                context.getString(R.string.food_rokumonya_schedule),
+                context.getString(R.string.food_price_two),
                 R.drawable.shop_rokumonya_okonomiyaki
         ));
 
         list.add(new Location(
-                "Kisoji",
-                "Restaurant specialised in sukiyaki and shabu shabu",
-                "3-17-5 New Fuji Building, Shinjuku, Tokyo, 160-0022",
-                "+81 3-3226-0667",
-                "Mo-Su 11:30–14:30 and 17:30-21:30",
-                "$$$$",
+                context.getString(R.string.food_kisoji_name),
+                context.getString(R.string.food_kisoji_description),
+                context.getString(R.string.food_kisoji_address),
+                context.getString(R.string.food_kisoji_phone),
+                context.getString(R.string.food_kisoji_schedule),
+                context.getString(R.string.food_price_four),
                 R.drawable.food_kisoji_shabushabu
         ));
 
         list.add(new Location(
-                "Gogo curry",
-                "Restaurant specialised in Japanese curry with fried meat on top",
-                "1 Chome−161, Kanda Sakumacho, Chiyoda, Tokyo, 101-0025",
-                "+81 3-5256-5525",
-                "Mo-Su 10:00–22:00",
-                "$",
+                context.getString(R.string.food_gogo_name),
+                context.getString(R.string.food_gogo_description),
+                context.getString(R.string.food_gogo_address),
+                context.getString(R.string.food_gogo_phone),
+                context.getString(R.string.food_gogo_schedule),
+                context.getString(R.string.food_price_one),
                 R.drawable.food_gogo_curry
         ));
 
         list.add(new Location(
-                "Tenya",
-                "Restaurant specialised in Tendon, a bowl of rice with tempura fried food on top",
-                "3 Chome−3−31−2, Shinjuku, Tokyo, 160-0022",
-                "+81 3-5269-7216",
-                "Mo-Su 11:00–22:00",
-                "$",
+                context.getString(R.string.food_tenya_name),
+                context.getString(R.string.food_tenya_description),
+                context.getString(R.string.food_tenya_address),
+                context.getString(R.string.food_tenya_phone),
+                context.getString(R.string.food_tenya_schedule),
+                context.getString(R.string.food_price_one),
                 R.drawable.food_tenya
         ));
 
         list.add(new Location(
-                "Shitukatsu",
-                "Tonkatsu place run by Agetarou's family. Agetarou is also known as Tonkatsu DJ.",
-                "21−6, Udagawacho, Shibuya, Tokyo, 150-0042",
-                "+81 3-0000-0000",
-                "Mo-Su 11:00–22:00",
-                "$",
+                context.getString(R.string.food_shibukatsu_name),
+                context.getString(R.string.food_shibukatsu_description),
+                context.getString(R.string.food_shibukatsu_address),
+                context.getString(R.string.food_shibukatsu_phone),
+                context.getString(R.string.food_shibukatsu_schedule),
+                context.getString(R.string.food_price_one),
                 R.drawable.food_shibukatsu
         ));
-
     }
-
 }
--- app/src/main/java/eu/laramartin/tokyoguide/FoodFragment.java
+++ app/src/main/java/eu/laramartin/tokyoguide/FoodFragment.java
@@ -21,7 +21,7 @@ public class FoodFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         List<Location> list = new ArrayList<>();
-        Food.initFoodsList(list);
+        Food.initFoodsList(list, getContext());
 
         LocationAdapter adapter = new LocationAdapter(getActivity(), -1, list);
         View view = inflater.inflate(R.layout.locations_list, container, false);
